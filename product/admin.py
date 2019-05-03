d6559b313c1e0f5fdcb96d09ce4133d7ef14b1dc
ankush953 <ankushraghuvanshi07@gmail.com>
2019-05-03 23:12:48 +0530

    added phonebrand model

--- product/admin.py
+++ product/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
 #import apps from models
-from product.models import Product, Review
+from product.models import Product, Review, PhoneBrand
 
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Review)
+admin.site.register(PhoneBrand)
 
 
--- product/models.py
+++ product/models.py
@@ -1,19 +1,23 @@
 from django.db import models
 
-# class PhoneBrand(models.Model):
-#     brand = models.CharField(max_length=100)
-#     country = models.CharField(max_length=100)
+class PhoneBrand(models.Model):
+    brand = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.brand
 
 # function to define where images will be stored
 def phone_gallery(request,filename):
-    return request.brand+'/'+request.name+'.jpg'
+    return request.brand.__str__() +'/'+request.name+'.jpg'
 
 # Create your models here.
 class Product(models.Model):
     # Attributes of product
 
-    brand = models.CharField(max_length=100)                         # Company of product
-    category = models.CharField(max_length=100)                      # Category
+    brand = models.ForeignKey(PhoneBrand,on_delete=models.CASCADE,
+    blank=True)                                                      # Company of product
+    # category = models.CharField(max_length=100)                      # Category
     name = models.CharField(max_length=100)                          # Name of product
     price = models.DecimalField(max_digits=10, decimal_places=2)     # price of product
     ram = models.IntegerField()                                      # ram of smartphone
@@ -27,7 +31,7 @@ class Product(models.Model):
     
 
     def __str__(self):
-        return self.brand + ' ' + self.name
+        return self.brand.__str__() + ' ' + self.name
 
 class Review(models.Model):
     product = models.ForeignKey(Product,on_delete=models.CASCADE)
